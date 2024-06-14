e3ef568a9412d0399366c7b2835e0f61a69960b6
Alina Ovod <alina.ipatova98@gmail.com>
2024-06-14 22:47:27 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,16 @@
+"use strict";
+const listItems = [
+    ...document.querySelectorAll("li")
+];
+const liWithUl = listItems.filter((item)=>item.querySelector("ul"));
+liWithUl.forEach((item)=>{
+    const span = document.createElement("span");
+    item.prepend(span);
+    span.append(item.childNodes[1]);
+    span.addEventListener("click", (e)=>{
+        const ul = item.querySelector("ul");
+        if (ul) ul.hidden = !ul.hidden;
+    });
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
